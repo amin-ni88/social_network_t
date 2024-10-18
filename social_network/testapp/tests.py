e3fc712ecbb8048fb933d7f279ecb936b36d6c98
itsMajid <itsMajid.dev@gmail.com>
2024-10-18 19:49:06 +0330

    create test_load_time

--- social_network/testapp/tests.py
+++ social_network/testapp/tests.py
@@ -7,10 +7,17 @@ class SocialTesting(TestCase):
     def setUp(self):
         pass
     
-    # def test_status_url(self):
-    #     request = self.client.get("/")
-    #     self.assertEqual(request.status_code , 200)
+    def test_status_url(self): # تست صحت صفحه نخست
+        request = self.client.get("/")
+        self.assertEqual(request.status_code , 200)
 
-    def test_404(self):
+    def test_404(self): # تست خطای 404 در صورت عدم وجود url
         request = self.client.get("ERitrokmM/")
         self.assertEqual(request.status_code , 404)
+
+    def test_load_time(self): # تست زمان لود صفحه (مد نظر 1.7 ثانیه زمان ببرد)
+        start_load = time()
+        request = self.client.get("/")
+        end_load = time()
+        load_time = end_load - start_load 
+        self.assertLess(load_time , 1.7)
