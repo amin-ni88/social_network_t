8894e3902de00a039cf940d860d6498a93b746b8
itsMajid <itsMajid.dev@gmail.com>
2024-10-18 19:38:25 +0330

    create test_status_url

--- social_network/testapp/tests.py
+++ social_network/testapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from time import time
 # Create your tests here.
+
+
+class SocialTesting(TestCase):
+    def setUp(self):
+        pass
+    
+    def test_status_url(self):
+        request = self.client.get("/")
+        self.assertEqual(request.status_code , 200)
