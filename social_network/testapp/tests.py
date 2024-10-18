d0c6090fc1290a24a80bbfdb7b3664dadb727b8f
itsMajid <itsMajid.dev@gmail.com>
2024-10-18 19:41:01 +0330

    create test_404

--- social_network/testapp/tests.py
+++ social_network/testapp/tests.py
@@ -7,6 +7,10 @@ class SocialTesting(TestCase):
     def setUp(self):
         pass
     
-    def test_status_url(self):
-        request = self.client.get("/")
-        self.assertEqual(request.status_code , 200)
+    # def test_status_url(self):
+    #     request = self.client.get("/")
+    #     self.assertEqual(request.status_code , 200)
+
+    def test_404(self):
+        request = self.client.get("ERitrokmM/")
+        self.assertEqual(request.status_code , 404)
